017217e559870fcb7cad6cb501459c4ac6d1df2a
Chris Otto <chris.otto6@gmail.com>
2021-10-25 09:29:01 -0500

    Resolves #1 - Standing up the repository and the main entry point for the proxy server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
